f17691922e70fb7443e709a862715a43755e92d2
Ruediger zu Dohna <snackbox@sinntr.eu>
2021-12-24 13:16:17 +0100

    improve docs on DI

--- junit/src/main/java/com/github/t1/wunderbar/junit/consumer/SystemUnderTest.java
+++ junit/src/main/java/com/github/t1/wunderbar/junit/consumer/SystemUnderTest.java
@@ -5,12 +5,11 @@ import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * The consumer code that has a field (generally annotated as <code>{@link javax.inject.Inject @Inject}</code>)
- * with an API interface ({@link org.eclipse.microprofile.rest.client.inject.RegisterRestClient RegisterRestClient} or
- * {@link io.smallrye.graphql.client.typesafe.api.GraphQLClientApi GraphQLClientApi})
- * and that a {@link WunderBarApiConsumer} test tests.
- * In some cases it may make sense to test only the the API interface and the DTOs, and not the code that
- * actually consumes that API, so it's not necessary to declare a {@link SystemUnderTest}. Or you could have several.
+ * The {@link WunderBarApiConsumer} extension will do basic dependency injection for fields annotated as SUT, namely
+ * inject those beans it has created for the fields annotated as {@link Service}.
+ * If you need more sophisticated dependency injection, i.e. other types, deeply nested classes, interceptors, etc.,
+ * you can use a full dependency injection (test) framework, e.g., <code>weld-junit5</code>.
+ * For more details see the <a href="https://github.com/t1/wunderbar#full-dependency-injection">README</a>.
  */
 @Retention(RUNTIME)
 public @interface SystemUnderTest {}
