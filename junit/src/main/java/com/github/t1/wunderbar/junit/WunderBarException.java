61ac88d7bc23a9840d82fddbf13c8ed069eb5c6f
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-01-28 09:07:57 +0100

    fix build

--- junit/src/main/java/com/github/t1/wunderbar/junit/WunderBarException.java
+++ junit/src/main/java/com/github/t1/wunderbar/junit/WunderBarException.java
@@ -2,5 +2,7 @@ package com.github.t1.wunderbar.junit;
 
 /** Something was wrong with the test code setup */
 public class WunderBarException extends IllegalArgumentException {
-    public WunderBarException(String message) { super(message); }
+    public WunderBarException(String message) {super(message);}
+
+    public WunderBarException(String message, Throwable cause) {super(message, cause);}
 }
