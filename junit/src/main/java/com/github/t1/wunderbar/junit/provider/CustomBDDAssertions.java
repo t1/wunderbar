cff21a59b4115aac5de80c78583aeca7959874ac
Ruediger zu Dohna <snackbox@sinntr.eu>
2022-01-28 05:20:56 +0100

    restore CustomBDDAssertions and JsonValueAssert for backward compatibility

---
+++ junit/src/main/java/com/github/t1/wunderbar/junit/provider/CustomBDDAssertions.java
@@ -0,0 +1,10 @@
+package com.github.t1.wunderbar.junit.provider;
+
+/**
+ * This remains here only for backward compatibility; use {@link WunderBarBDDAssertions} instead.
+ * <p>
+ * TODO 3.0: breaking change: remove
+ */
+@Deprecated(forRemoval = true)
+public class CustomBDDAssertions extends WunderBarBDDAssertions {
+}
---
+++ junit/src/main/java/com/github/t1/wunderbar/junit/provider/JsonValueAssert.java
@@ -0,0 +1,13 @@
+package com.github.t1.wunderbar.junit.provider;
+
+import javax.json.JsonValue;
+
+/**
+ * This remains here only for backward compatibility; use {@link com.github.t1.wunderbar.junit.assertions.JsonValueAssert} instead.
+ * <p>
+ * TODO 3.0: breaking change: remove
+ */
+@Deprecated(forRemoval = true)
+public class JsonValueAssert extends com.github.t1.wunderbar.junit.assertions.JsonValueAssert<JsonValueAssert, JsonValue> {
+    public JsonValueAssert(JsonValue jsonValue) {super(jsonValue);}
+}
